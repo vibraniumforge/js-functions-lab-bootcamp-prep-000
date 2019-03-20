4cdb5c18263d88d8e09fa3738d6cc9ad4560f967
Greg Morgan <gregm9999@gmail.com>
2019-03-20 21:34:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidaysTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return "It's #{days} days until ${holiday}"
+  return "It's #{days} days until ${holiday}";
 }
