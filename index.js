537bb707981dd7fba1b17e0a001fd291ea58c191
Greg Morgan <gregm9999@gmail.com>
2019-03-20 21:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
+function happyHolidaysTo(name) {
+  return `Happy holiday, ${name}!`;
+}
+
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
